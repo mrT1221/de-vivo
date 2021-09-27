5095f008acca5b1c5dff0035a3a3774d1d9b0d56
tele-vivo <91408830+tele-vivo@users.noreply.github.com>
2021-09-27 16:13:25 +0700

    Update canales.js

--- canales.js
+++ canales.js
@@ -2,5 +2,13 @@ const Canales = [{
         'nombre': '🏛️ sky sports envivo',
         'url': '/2021/09/sky-sports-en-vivo.html',
         'imgUrl': 'https://1.bp.blogspot.com/-3pYdimZ8Io0/YUrqoaouYwI/AAAAAAAACzo/b7BloBvN3dU_mbRq45nun0daxRovN5P3wCLcBGAsYHQ/s200/New-Sky-Sports-logo-2020.png'
+    },
+        'nombre': '🏛️ ESPN Mexico',
+        'url': '/2021/09/espn-mexico-en-vivo.html',
+        'imgUrl': 'https://tele-vivo.github.io/de-vivo/ESPN.png'
+    },
+        'nombre': '🏛️ ESPN 2 Mexico',
+        'url': '/2021/09/ESPN-2-Mexico-En-Vivo.html',
+        'imgUrl': 'https://tele-vivo.github.io/de-vivo/ESPN_2.png'
     }
 ]
